33c2e697ab33e53025f2431e4fd764db70819775
sagarstha254 <sagarstha254@gmail.com>
2021-12-12 23:45:35 +0545

    experience

---
+++ hello.js
@@ -0,0 +1,12 @@
+console.log("hello");
+//let a = 7 
+//let b = 6
+//let sum = a + b
+let name = prompt("enter a name");
+if (name === "sagar"){
+    alert("hello");
+}
+else {
+    alert("get lost");
+}
+
